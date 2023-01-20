ff4bb5540ea506be1b150a204885d25b69996f65
kygodoss <kygodoss@gmail.com>
2023-01-20 21:36:25 +0900

    게시물 작성 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -9,13 +9,41 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.printf("명령어 ) ");
-		String cmd;
+		int lastArticleID = 0;
 
-		cmd = sc.next();
-		System.out.printf("입력된 명령어 : %s\n", cmd);
-		int num = sc.nextInt();
-		System.out.printf("입력된 정수 : %d\n", num);
+		while (true) {
+			System.out.printf("명령어 ) ");
+			String cmd = sc.nextLine();
+
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력하세요");
+				continue;
+			}
+
+			if (cmd.equals("system exit")) {
+				break;
+			}
+
+			if (cmd.equals("article write")) {
+				int id = lastArticleID + 1;
+				lastArticleID = id;
+
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+//				System.out.printf("%s, %s\n", title, body);
+				System.out.printf("%d번글이 생성되었습니다\n",id);
+				
+				
+			} else if (cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다.");
+				
+			} else {
+				System.out.println("없는 명령어입니다");
+			}
+		}
 
 		sc.close();
 		System.out.println(" == 프로그램 종료 ==");
