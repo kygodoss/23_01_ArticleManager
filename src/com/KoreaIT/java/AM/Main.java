52cfb00271aa3e3fe1424fc0ec511f31013e2918
kygodoss <kygodoss@gmail.com>
2023-01-26 21:03:36 +0900

    게시물 상세페이지 구현, 날짜는 아직

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -42,11 +42,42 @@ public class Main {
 				System.out.printf("%d번글이 생성되었습니다\n", id);
 
 			} else if (cmd.equals("article list")) {
-				if(articles.size() == 0) {
+				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다.");
+					continue;
 				}
-				else {System.out.println("게시글이 존재.");
+				System.out.println("번호		|		제목");
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d		|		%s\n", article.id, article.title);
 				}
+
+			} else if (cmd.startsWith("article detail ")) {
+				String[] cmdBits = cmd.split(" ");
+
+				int id = Integer.parseInt(cmdBits[2]);
+
+				Article foundArticle = null;
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
+				}
+
+				System.out.printf("%d번 게시물은 존재합니다\n", id);
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : 2023-12-09 12:12:12\n");
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+				
 			} else {
 				System.out.println("없는 명령어입니다");
 			}
@@ -61,7 +92,7 @@ class Article {
 	int id;
 	String title;
 	String body;
-	
+
 	public Article(int id, String title, String body) {
 		this.id = id;
 		this.title = title;
