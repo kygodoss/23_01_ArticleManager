b099c98461ec387bef19aaa41b512cde30ff5b7b
kygodoss <kygodoss@gmail.com>
2023-01-31 20:50:56 +0900

    조회수 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -37,10 +37,11 @@ public class Main {
 					System.out.println("게시글이 없습니다");
 					continue;
 				}
-				System.out.println("번호		|		제목");
+				
+				System.out.println("번호    |  제목  |    조회");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
-					System.out.printf("%d		|		%s\n", article.id, article.title);
+					System.out.printf("%4d  | %4s |  %4d\n", article.id, article.title, article.viewCnt);
 				}
 			} else if (cmd.startsWith("article detail ")) {
 				String[] cmdBits = cmd.split(" ");
@@ -57,11 +58,13 @@ public class Main {
 					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
 					continue;
 				}
-				System.out.printf("%d번 게시물은 존재합니다\n", id);
+				
+				foundArticle.increaseViewCnt();
 				System.out.printf("번호 : %d\n", foundArticle.id);
 				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
+				System.out.printf("조회 : %d\n", foundArticle.viewCnt);
 
 			} else if (cmd.startsWith("article modify ")) {
 				String[] cmdBits = cmd.split(" ");
@@ -130,10 +133,15 @@ class Article {
 	String regDate;
 	String title;
 	String body;
+	int viewCnt;
 	public Article(int id, String regDate, String title, String body) {
 		this.id = id;
 		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
+		this.viewCnt = 0;
+	}
+	public void increaseViewCnt() {
+		viewCnt++;
 	}
 }
